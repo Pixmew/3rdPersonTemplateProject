e023cc9802eca5f523e36e8ce832687496c6181c
Yash Shete <universeyash4@gmail.com>
2021-05-01 19:13:53 +0530

    Controller Logic Changed and Project Organization
    In the Controller Class the logic of Movement  and Rotation is Seperated into two different functions
    
    All the Scripts in project is moved to the Scripts Folder and animation into animation folder with "test"  (animation controller) renamed as "CharacterAnimations" and skybox is moved to Resources folder

--- FunExperimentsProjects/Assets/Scripts/Controller.cs
+++ FunExperimentsProjects/Assets/Scripts/Controller.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Controller : MonoBehaviour
 {
     [SerializeField]
@@ -16,36 +18,53 @@ public class Controller : MonoBehaviour
     void LateUpdate()
     {
         Move();
+        Look();
     }
+
 
     void Move()
     {
+        //if There is no Input 
         if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
         {
+            //set speed to zero and play Idle animation
             speed = 0;
             animator.SetBool("isIdle" , true);
-            animator.SetBool("isRunning", false);
-            animator.SetBool("isWalking", false);
+            //if Other animations is running stop it
+            if (animator.GetBool("isRunning") == true || animator.GetBool("isWalking"))
+            {
+                animator.SetBool("isRunning", false);
+                animator.SetBool("isWalking", false);
+            } 
         }
-        else 
+        else //if there is some input
         {
+            //if sprint/left-shift is pressed start Running
             if (Input.GetAxis("Sprint") > 0)
             {
                 speed = 5;
                 animator.SetBool("isRunning" , true);
             }
-            else
+            else//if forward button is pressed start walking
             {
-                animator.SetBool("isRunning", false);
-                animator.SetBool("isWalking", true);
                 speed = 2;
+                animator.SetBool("isWalking", true);
+                //if Running animations is running stop it
+                if (animator.GetBool("isRunning") == true)
+                {
+                    animator.SetBool("isRunning", false);
+                }  
             }
-            
+            //Moving Logic  
             Vector3 MoveDirectionNormal = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             transform.Translate(MoveDirectionNormal * speed * Time.deltaTime);
-        }
-        ang = new Vector3(0 , Input.GetAxis("Mouse X") , 0);
-        angcam = new Vector3(-Input.GetAxis("Mouse Y")*0.5f , 0 , 0);
+        } 
+    }
+    void Look()
+    {
+        //Looking Logic
+        ang = new Vector3(0, Input.GetAxis("Mouse X"), 0);
+        angcam = new Vector3(-Input.GetAxis("Mouse Y") * 0.5f, 0, 0);
         transform.Rotate(ang);
         cam.transform.Rotate(angcam);
     }
